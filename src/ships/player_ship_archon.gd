2b002d1890dd8712b930ecddf66f7db2e50d00d1
B1773rm4n <info@nofunallworkgames.fyi>
2026-05-08 09:39:35 +0700

    Limit camera yaw with the ship and add vertical movement

--- src/ships/player_ship_archon.gd
+++ src/ships/player_ship_archon.gd
@@ -6,13 +6,17 @@ extends CharacterBody3D
 @export var look_sensitivity: float = 0.003
 @export var min_pitch_degrees: float = -45.0
 @export var max_pitch_degrees: float = 45.0
+@export var camera_yaw_limit_degrees: float = 170.0
 
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera_3d: Camera3D = $CameraPivot/SpringArm3D/Camera3D
 
 var _look_pitch: float = 0.0
+var _camera_yaw: float = 0.0
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	_camera_yaw = camera_pivot.rotation.y
 
 func _physics_process(delta: float) -> void:
 	_apply_movement(delta)
@@ -24,12 +28,31 @@ func _physics_process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	# Mouse movement
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * look_sensitivity)
+		var yaw_delta: float = -event.relative.x * look_sensitivity
+		var desired_yaw: float = _camera_yaw + yaw_delta
+
+		# Let the camera freelook, but when it gets close to looking "back" the ship,
+		# rotate the ship instead so controls don't feel inverted.
+		var yaw_limit: float = deg_to_rad(camera_yaw_limit_degrees)
+		if desired_yaw > yaw_limit:
+			var pivot_delta: float = yaw_limit - _camera_yaw
+			var ship_delta: float = yaw_delta - pivot_delta
+			_camera_yaw = yaw_limit
+			rotate_y(ship_delta)
+		elif desired_yaw < -yaw_limit:
+			var pivot_delta: float = -yaw_limit - _camera_yaw
+			var ship_delta: float = yaw_delta - pivot_delta
+			_camera_yaw = -yaw_limit
+			rotate_y(ship_delta)
+		else:
+			_camera_yaw = desired_yaw
+
 		_look_pitch = clampf(
 			_look_pitch - event.relative.y * look_sensitivity,
 			deg_to_rad(min_pitch_degrees),
 			deg_to_rad(max_pitch_degrees)
 		)
+		camera_pivot.rotation.y = _camera_yaw
 		camera_pivot.rotation.x = _look_pitch
 	# if the mouse is clicked, capture the mouse
 	elif event is InputEventMouseButton and event.pressed:
@@ -45,14 +68,16 @@ func _apply_movement(delta: float) -> void:
 	var forward_input := Input.get_action_strength("ship_forward") - Input.get_action_strength("ship_back")
 	# same but for sideways movement
 	var strafe_input := Input.get_action_strength("ship_strafe_right") - Input.get_action_strength("ship_strafe_left")
+	var vertical_input := Input.get_action_strength("ship_ascend") - Input.get_action_strength("ship_descend")
 
 	var target_velocity: Vector3 = Vector3.ZERO
-	var move_input := Vector2(strafe_input, forward_input)
-	if move_input.length_squared() > 0.0:
-		move_input = move_input.normalized()
+	var local_dir: Vector3 = Vector3(strafe_input, vertical_input, -forward_input)
+	if local_dir.length_squared() > 0.0:
+		local_dir = local_dir.normalized()
 		target_velocity = (
-			global_basis.x * move_input.x +
-			-global_basis.z * move_input.y
+			global_basis.x * local_dir.x +
+			global_basis.y * local_dir.y +
+			global_basis.z * local_dir.z
 		) * max_speed
 
 	var blend_weight := acceleration if target_velocity != Vector3.ZERO else deceleration
