5a1f610cdc9f10954a47b60d641f27bd350bcb27
B1773rm4n <info@nofunallworkgames.fyi>
2026-05-25 21:52:06 +0700

    Change asteroid spawn to the start of the game and fix cargo pickup

--- src/components/cargo_component.gd
+++ src/components/cargo_component.gd
@@ -8,9 +8,6 @@ func _ready() -> void:
 	SignalBus.player_resource_received.connect(_on_player_resource_received)
 	SignalBus.player_resource_received_view_update.emit(data.cargo_amount)
 
-func _exit_tree() -> void:
-	SignalBus.player_resource_received.disconnect(_on_player_resource_received)
-
 func _on_player_resource_received(amount: int) -> void:
 	data.add_cargo(amount)
 	SignalBus.player_resource_received_view_update.emit(data.cargo_amount)
--- src/spawner/gaussian_asteroid.gd
+++ src/spawner/gaussian_asteroid.gd
@@ -1,36 +1,24 @@
-@tool
 extends Area3D
 
-@export_tool_button("Create Asteroids", "CreateIcon") var create_button = create_asteroids
-@export_tool_button("Clear Asteroids", "ClearIcon") var clear_button = clear_asteroids
 @export_group("Field Settings")
-@export var count: int = 100
-@export var spread: float = 50.0 # Replaces noise_strength
+@export var count: int = 200
+@export var spread: float = 200.0
 
 @export_group("Asteroid Settings")
 @export var max_health: float = 100.0
-@export var health: float = 100.0
+@export var health: float = 50.0
 
 const asteroid_scene := preload("res://scenes/Objects/asteroid.tscn")
 
 func _ready() -> void:
-	if not Engine.is_editor_hint():
-		SignalBus.damage_asteroid.connect(_on_damage_asteroid)
+	SignalBus.damage_asteroid.connect(_on_damage_asteroid)
+	spawn_gaussian_cloud()
 
 func _on_damage_asteroid(target, damage) -> void:
 	if is_instance_valid(target) and target.has_method("take_damage"):
 		target.take_damage(damage)
 
-func create_asteroids() -> void:
-	clear_asteroids()
-	spawn_gaussian_cloud()
-
-func clear_asteroids() -> void:
-	for child in get_children():
-		child.free()
-
 func spawn_gaussian_cloud() -> void:
-
 	for i in range(count):
 		var direction = Vector3(
 			randfn(0, 1),
@@ -40,22 +28,12 @@ func spawn_gaussian_cloud() -> void:
 
 		var distance = randfn(0, spread)
 		var final_pos = direction * distance
-
-		# Add Data
-		health = 50.0
-		# First number is random cap, second number is always added
 		var gained_resource :float = randi() % 10 + 1
 
 		var asteroid_instance = asteroid_scene.instantiate()
 		asteroid_instance.transform.origin = final_pos
 		asteroid_instance.health = health
-		asteroid_instance.max_health = health
+		asteroid_instance.max_health = max_health
 		asteroid_instance.gained_resource = gained_resource
 		add_child(asteroid_instance)
-
-		if Engine.is_editor_hint():
-			var root = get_tree().edited_scene_root
-			asteroid_instance.owner = root
-
-		# Group assignment must occur after owner assignment and use the persistent flag
-		asteroid_instance.add_to_group("Asteroid", true)
+		asteroid_instance.add_to_group("Asteroid")
