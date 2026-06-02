924d7028384ab6355b58232ed22342fc50655364
B1773rm4n <info@nofunallworkgames.fyi>
2026-06-02 09:35:45 +0700

    Add speed meter to make it more obvious when the ship dies because of overspeed

--- src/ui/healthbar_ui.gd
+++
@@ -1,9 +0,0 @@
-extends MarginContainer
-
-@export var progress_bar: ProgressBar
-
-func _ready() -> void:
-	SignalBus.player_receive_damage.connect(_on_damage_received)
-
-func _on_damage_received(amount: int):
-	progress_bar.value -= amount
---
+++ src/ui/player_ship_hud.gd
@@ -0,0 +1,23 @@
+extends MarginContainer
+
+@export var health_progress_bar: ProgressBar
+@export var speed_progress_bar: ProgressBar
+
+func _ready() -> void:
+	SignalBus.player_receive_damage.connect(_on_player_receive_damage)
+	SignalBus.update_ui.connect(_sync_speed_bar_max)
+	_sync_speed_bar_max()
+
+func _process(_delta: float) -> void:
+	_update_speed_bar()
+
+func _sync_speed_bar_max() -> void:
+	var player := GameManager.PlayerShip
+	speed_progress_bar.max_value = player.max_speed
+
+func _update_speed_bar() -> void:
+	var player := GameManager.PlayerShip
+	speed_progress_bar.value = player.speed
+
+func _on_player_receive_damage(amount: int) -> void:
+	health_progress_bar.value -= amount
