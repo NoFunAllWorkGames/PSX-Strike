c2d4f6646ca68ec96a6da779e8fc07227ce34164
vehN <bbeddell@gmail.com>
2026-06-13 23:53:19 +0800

    Updated Station Hotkey Display

--- src/scenes/space_station_outside.gd
+++ src/scenes/space_station_outside.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var _player_in_dock_zone: bool = false
+
+var hotkey_array = InputMap.action_get_events("interact")
+var interact_hotkey = hotkey_array[0].as_text_physical_keycode()
+
 @export var space_station_entry_area: Area3D
 
 func _ready() -> void:
@@ -18,7 +22,8 @@ func _on_interact_pressed() -> void:
 
 func _on_area_3d_body_entered(_body: Node3D) -> void:
 	_player_in_dock_zone = true
-	SignalBus.display_action_label.emit("Press G to enter")
+	print(hotkey_array)
+	SignalBus.display_action_label.emit("Press %s to enter" %interact_hotkey )
 
 func _on_area_3d_body_exited(_body: Node3D) -> void:
 	_player_in_dock_zone = false
