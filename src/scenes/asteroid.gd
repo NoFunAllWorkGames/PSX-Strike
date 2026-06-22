b8f52cd7261b309d3911016056b20167787eb191
B1773rm4n <info@nofunallworkgames.fyi>
2026-06-22 13:24:52 +0700

    Add collision sound between ship and asteroids

--- src/scenes/asteroid.gd
+++ src/scenes/asteroid.gd
@@ -9,25 +9,53 @@ class_name Asteroid
 @export var mesh_seed: int = -1
 @export var mesh_radius: float = 1.0
 @export var is_precious: bool = false
+@export var hull_contact_area: Area3D
 
 const ProceduralAsteroidMeshBuilder := preload("res://src/utils/procedural_asteroid_mesh.gd")
+const ROCK_SOUNDS: Array[AudioStream] = [
+	preload("res://assets/Sounds/Collisions/WithRocks/CollisionRock1.ogg"),
+	preload("res://assets/Sounds/Collisions/WithRocks/CollisionRock2.ogg"),
+]
+
+const HULL_CONTACT_SCALE := 1.5
+
 var asteroid_pickup = preload("res://scenes/Objects/asteroid_pickup.tscn")
 
 @onready var _mesh_instance: MeshInstance3D = $MeshInstance3D
 @onready var _collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var _hull_contact_shape: CollisionShape3D = $HullContact/HullContact_CollisionShape3D
 @onready var _damage_bar = $DamageBar
+@onready var _collision_audio: AudioStreamPlayer = $CollisionAudio
 
 
 func _ready() -> void:
 	if mesh_seed < 0:
 		mesh_seed = randi()
 	_apply_procedural_mesh()
+	hull_contact_area.body_entered.connect(_on_hull_contact_play_sound)
+
+
+func _exit_tree() -> void:
+	hull_contact_area.body_entered.disconnect(_on_hull_contact_play_sound)
+
+
+func _on_hull_contact_play_sound(body: Node3D) -> void:
+	if body != GameManager.PlayerShip:
+		return
+
+	_collision_audio.stream = ROCK_SOUNDS.pick_random()
+	_collision_audio.play()
 
 
 func _apply_procedural_mesh() -> void:
 	var mesh: ArrayMesh = ProceduralAsteroidMeshBuilder.build(mesh_seed, mesh_radius)
+	var convex_shape: ConvexPolygonShape3D = mesh.create_convex_shape()
 	_mesh_instance.mesh = mesh
-	_collision_shape.shape = mesh.create_convex_shape()
+	_collision_shape.shape = convex_shape
+	_collision_shape.scale = Vector3.ONE
+
+	_hull_contact_shape.shape = convex_shape
+	_hull_contact_shape.scale = Vector3.ONE * HULL_CONTACT_SCALE
 
 func take_damage(applied_damage: float) -> void:
 	health -= applied_damage
